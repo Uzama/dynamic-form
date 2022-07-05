040c142d843b1975e8fe30b2c45cbcf26f1775d3
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-07-05 11:51:59 +0530

    bug fixed in css style overide for material ui in production build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import type { AppProps } from "next/app";
 import { store } from "../app/Store";
+import Head from "next/head";
 import { Provider } from "react-redux";
+import CssBaseline from "@mui/material/CssBaseline";
+import createEmotionCache from "../components/createEmotionCache";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+// source: https://github.com/mui/material-ui/tree/master/examples/nextjs-with-typescript
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </CacheProvider>
     </Provider>
   );
 }
-
-export default MyApp;
